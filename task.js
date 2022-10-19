3d8b34f124383afec1ecb2257e26ec08246549e6
Anatoliy Oleynikov <oleinikovav.ao@gmail.com>
2022-10-19 10:33:52 +0300

    Task1

---
+++ task.js
@@ -0,0 +1,22 @@
+class PrintEditionItem {
+  constructor(name, releaseDate, pagesCount, state = 100, type = null){
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.pagesCount = pagesCount;
+    this.state = state;
+  }
+  fix(){
+    this.state = this.state * 1.5;
+  }
+  set state(state) {
+    if (state < 0){
+      this._state = 0;      
+    } else {
+      this._state = 100;
+    }
+  
+  }
+  get state(){
+    return this._state;
+  }
+}
